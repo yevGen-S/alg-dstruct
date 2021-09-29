76cd249429009ca618f1e282818f5035cce9743f
Yevgeniy Sudakov <yevgeniysudakov@gmail.com>
2021-09-30 02:51:53 +0300

    feat: add and remake Unit Tests

--- 2-SudakovEvgenii-A10/UnitTests/UnitTests.cpp
+++ 2-SudakovEvgenii-A10/UnitTests/UnitTests.cpp
@@ -10,17 +10,34 @@ list_t* test_stack = CreateStack();
 TEST(testStackOnList, handleCreateStackOnListFun) {	
 	list_t* stack = CreateStack();
     EXPECT_TRUE(stack != NULL);
+	EXPECT_TRUE(stack->head->next == NULL);
+	EXPECT_TRUE(stack->length == 0);
+	EXPECT_TRUE(stack->top == stack->head);
 }
 
 TEST(testStackOnList, handlePushInStackOnListFun) {
 	Push(test_stack, 1);
 	EXPECT_TRUE(test_stack->top->data == 1);
+	EXPECT_TRUE(test_stack->head->next->data == 1);
+	EXPECT_TRUE(test_stack->length == 1);
 	Push(test_stack, 2);
+	EXPECT_TRUE(test_stack->top->data == 2);
+	EXPECT_TRUE(test_stack->head->next->next->data == 2);
+	EXPECT_TRUE(test_stack->length == 2);
+	Push(test_stack, 3);
+	EXPECT_TRUE(test_stack->top->data == 3);
+	EXPECT_TRUE(test_stack->head->next->next->next->data == 3);
+	EXPECT_TRUE(test_stack->length == 3);
+	Push(test_stack, 4);
+	EXPECT_TRUE(test_stack->top->data == 4);
+	EXPECT_TRUE(test_stack->length == 4);
 }
+
 
 TEST(testStackOnList, handlePopFromStackOnListFun) {
 	Pop(test_stack);
-	EXPECT_TRUE(test_stack->top->data == 1);
+	EXPECT_TRUE(test_stack->top->data == 3);
+	EXPECT_TRUE(test_stack->length == 3);
 }
 
 TEST(testStackOnList, handleWatchTopElementInStackOnListFun) {
@@ -29,5 +46,37 @@ TEST(testStackOnList, handleWatchTopElementInStackOnListFun) {
 
 TEST(testStackOnList, handleDeleteStackOnListFun) {
 	EXPECT_EQ(DeleteStack(test_stack), 0);
+}
+
+TEST(testStackOnArr, handlePushInStackOnArrFun) {
+	stack_on_arr_t stack_arr;
+	stack_arr.length = 0;
+	PushSA(&stack_arr, 1);
+	EXPECT_TRUE(stack_arr.arr[stack_arr.length - 1] == 1);
+	EXPECT_TRUE(stack_arr.length == 1);
+	PushSA(&stack_arr, 2);
+	EXPECT_TRUE(stack_arr.arr[stack_arr.length - 1] == 2);
+	EXPECT_TRUE(stack_arr.length == 2);
+	PushSA(&stack_arr, 3);
+	EXPECT_TRUE(stack_arr.arr[stack_arr.length - 1] == 3);
+	EXPECT_TRUE(stack_arr.length == 3);
+}
+
+TEST(testStackOnArr, handlePopFromStackOnArrFun) {
+	stack_on_arr_t stack_arr2;
+	stack_arr2.length = 0;
+	PushSA(&stack_arr2, 1);
+	PushSA(&stack_arr2, 2);
+	PopSA(&stack_arr2);
+	EXPECT_TRUE(stack_arr2.arr[stack_arr2.length - 1] == 1);
+}
+
+TEST(testStackOnArr, handleTopFromStackOnArrFun) {
+	stack_on_arr_t stack_arr3;
+	stack_arr3.length = 0;
+	PushSA(&stack_arr3, 1);
+	PushSA(&stack_arr3, 2);
+	PushSA(&stack_arr3, 3);
+	EXPECT_EQ(TopSA(&stack_arr3),stack_arr3.arr[stack_arr3.length - 1]);
 }
 
